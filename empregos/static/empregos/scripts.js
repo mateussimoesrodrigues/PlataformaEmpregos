1f7f83fd38e277213ab3e4a5cf7cc52fda220ff2
Mateus Simões <mateus.srodrigues@hotmail.com>
2025-02-27 00:13:57 -0300

    Criação inicial do html, css e js

---
+++ empregos/static/empregos/scripts.js
@@ -0,0 +1,4 @@
+// Função para mostrar uma mensagem no console quando o botão for clicado
+document.getElementById('testButton').addEventListener('click', function() {
+    alert('DOS AMIGOS!');
+});
